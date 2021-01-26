7486c125e15576c6bf154f720ca55c72248b322c
Aqshola <aqsholafid2@gmail.com>
2021-01-26 11:09:05 +0700

    create add data to database

--- controller/admin.js
+++ controller/admin.js
@@ -42,8 +42,100 @@ const getRelasi = async (req, res) => {
   }
 };
 
+const getAddRusak = async (req, res) => {
+  res.render("pages/action.ejs", { type: "rusak" });
+};
+
+const postAddRusak = async (req, res) => {
+  try {
+    const { kode_rusak, nama_rusak } = req.body;
+    await sequelize.query(
+      `insert into kerusakan values ('${kode_rusak}','${nama_rusak}')`,
+      {
+        type: QueryTypes.INSERT,
+      }
+    );
+    console.log("success");
+  } catch (err) {
+    console.log(err);
+  }
+
+  res.render("pages/action.ejs", { type: "rusak" });
+};
+
+const getAddGejala = async (req, res) => {
+  res.render("pages/action.ejs", { type: "gejala" });
+};
+
+const postAddGejala = async (req, res) => {
+  try {
+    const { kode_gejala, nama_gejala } = req.body;
+
+    await sequelize.query(
+      `insert into gejala values ('${kode_gejala}','${nama_gejala}')`,
+      {
+        type: QueryTypes.INSERT,
+      }
+    );
+    console.log("success");
+  } catch (err) {
+    console.log(err);
+  }
+
+  res.render("pages/action.ejs", { type: "gejala" });
+};
+
+const getAddRelasi = async (req, res) => {
+  const dataGejala = await sequelize.query("select * from gejala", {
+    type: QueryTypes.SELECT,
+  });
+
+  const dataRusak = await sequelize.query("select * from kerusakan", {
+    type: QueryTypes.SELECT,
+  });
+
+  console.log(dataGejala);
+  console.log(dataRusak);
+
+  res.render("pages/action.ejs", { type: "relasi", dataGejala, dataRusak });
+};
+
+const postAddRelasi = async (req, res) => {
+  try {
+    const { gejala_awal, ya, tidak } = req.body;
+
+    await sequelize.query(
+      `insert into pertanyaan (tanya_awal, ya, tidak) 
+       values ('${gejala_awal}','${ya}','${tidak}')`,
+      {
+        type: QueryTypes.INSERT,
+      }
+    );
+
+    console.log("success");
+
+    const dataGejala = await sequelize.query("select * from gejala", {
+      type: QueryTypes.SELECT,
+    });
+
+    const dataRusak = await sequelize.query("select * from kerusakan", {
+      type: QueryTypes.SELECT,
+    });
+
+    res.render("pages/action.ejs", { type: "relasi", dataGejala, dataRusak });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 module.exports = {
   getRusak,
   getGejala,
   getRelasi,
+  getAddRusak,
+  getAddGejala,
+  getAddRelasi,
+  postAddRusak,
+  postAddGejala,
+  postAddRelasi,
 };
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,29 @@
 const express = require("express");
-const { getRusak, getGejala, getRelasi } = require("../controller/admin");
+const {
+  getRusak,
+  getGejala,
+  getRelasi,
+  getAddRusak,
+  getAddGejala,
+  getAddRelasi,
+  postAddRusak,
+  postAddGejala,
+  postAddRelasi,
+} = require("../controller/admin");
 
 const router = express.Router();
 
 router.get("/", getRusak);
 router.get("/gejala", getGejala);
 router.get("/relasi", getRelasi);
+
+router.get("/add/rusak", getAddRusak);
+router.post("/add/rusak", postAddRusak);
+
+router.get("/add/gejala", getAddGejala);
+router.post("/add/gejala", postAddGejala);
+
+router.get("/add/relasi", getAddRelasi);
+router.post("/add/relasi", postAddRelasi);
 
 module.exports = router;
